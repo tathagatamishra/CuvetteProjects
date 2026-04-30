5696d4afb14526690f35529dcf0f3d02f3eb4c2b
Tathagata Mishra <tathagatamishra007@gmail.com>
2026-05-01 02:59:30 +0530

    update

--- PersonalPortfolio/fonts/convert-font.js
+++ PersonalPortfolio/fonts/convert-font.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
 const woff2 = require("woff2");
 
-const inputPath = "./fonts/WsParadose.ttf";
-const outputPath = "./fonts/WsParadose.woff2";
+const inputPath = "./WsParadose.ttf";
+const outputPath = "./WsParadose.woff2";
 
 const input = fs.readFileSync(inputPath);
 const output = woff2.encode(input);
